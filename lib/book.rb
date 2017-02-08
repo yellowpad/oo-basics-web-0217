9587c5e929e6ea5f0cdcfb634bc73434ca89088e
dgwp-projects <diggable.co@gmail.com>
2017-02-08 17:05:19 -0500

    Learn submit

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,23 @@
+class Book
+	attr_accessor(:title, :genre, :author, :page_count)
 
+	def initialize(title)
+		@title = title
+	end
 
+	def title
+		@title
+  	end
+
+  	def author
+  		@author
+  	end
+
+  	def genre
+  		@genre
+  	end
+  	
+  	def turn_page()
+  		puts "Flipping the page...wow, you read fast!"
+  	end
+end
